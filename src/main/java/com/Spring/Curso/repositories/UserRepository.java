1ba908aab1314cdea12ea4f3774c5b93fb821c14
THE ONE ABOVE ALL <nelsonred100@gmail.com>
2022-08-17 21:56:01 -0300

    Let's go

---
+++ src/main/java/com/Spring/Curso/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.Spring.Curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Spring.Curso.entitites.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	
+	
+	
+}
